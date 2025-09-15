31947b3d67df01bf030cefab7e3483a07462efc4
John Alencar <john.wesley.alencar2@gmail.com>
2025-09-15 17:52:09 -0300

    Esqueleto dashboard finalizado

--- frontend/agileboard-frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/agileboard-frontend/src/app/pages/dashboard/dashboard.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { StatCard } from '../../components/stat-card/stat-card';
 import { SprintProgressCard } from '../../components/sprint-progress-card/sprint-progress-card';
 import { SquadRankingCard } from '../../components/squad-ranking-card/squad-ranking-card'
-
+import { ProjectStatusCard } from '../../components/project-status-card/project-status-card';
+import { ActivityFeedCard } from '../../components/activity-feed-card/activity-feed-card';
+import { BurndownChartCard } from '../../components/burndown-chart-card/burndown-chart-card';
 @Component({
   selector: 'app-dashboard',
-  imports: [StatCard, SprintProgressCard, SquadRankingCard],
+  imports: [StatCard, SprintProgressCard, SquadRankingCard, ProjectStatusCard, ActivityFeedCard, BurndownChartCard],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
